2ea0a4d72d3ec5ea3225d58cbc818dddedf0e920
adiraimaji <adiraimaji@gmail.com>
2026-08-06 05:20:32 +0530

    Version Number Changed

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.adiraimaji.customkeyboard"
     minSdk = 21
     targetSdk { version = release(36) }
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.0.1"
   }
 
   sourceSets {
