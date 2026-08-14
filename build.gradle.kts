e569879d1f6971e608d9cae1d5612dcf8525d28c
adiraimaji <adiraimaji@gmail.com>
2026-08-14 20:34:23 +0530

    Release 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ android {
     applicationId = "com.adiraimaji.customkeyboard"
     minSdk = 21
     targetSdk { version = release(36) }
-    versionCode = 5
-    versionName = "1.2.0"
+    versionCode = 6
+    versionName = "1.2.1"
   }
 
   sourceSets {
